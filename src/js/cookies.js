3aeadee1d7f68d3c2bb6471134b74455bc68d126
CSKim <peurocs4@gmail.com>
2020-07-22 18:08:25 +0900

    Added webchat setting dialog and cookie
    - Added an webchat setting dialog
    - Added a function to save/load setting from the cookie

---
+++ src/js/cookies.js
@@ -0,0 +1,53 @@
+class CookieUtil {
+  static instance = null;
+
+  /**
+   * @return {null|CookieUtil}
+   */
+  constructor() {
+    if (CookieUtil.instance !== null) {
+      return CookieUtil.instance;
+    } else {
+      CookieUtil.instance = this;
+      return this;
+    }
+  }
+
+  /**
+   * @param {string} key
+   */
+  delete(key) {
+    this.set(key, "", 0);
+  }
+
+  /**
+   * @param {string} key
+   * @param {string} value
+   * @param {number} expireDays
+   */
+  set(key, value, expireDays= 1) {
+    let date = new Date();
+    date.setTime(date.getTime() + (expireDays * 24 * 60 * 60 * 1000));
+    document.cookie = `${key}=${value};expires=${date.toUTCString()};path=/`;
+  }
+
+  /**
+   * @param {string} key
+   * @return {string}
+   */
+  get(key) {
+    const keyString = `${key}=`;
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let cookieList = decodedCookie.split(';');
+    for (let i = 0; i < cookieList.length; ++i) {
+      let cookie = cookieList[i];
+      while (cookie[0] === ' ') {
+        cookie = cookie.substring(1);
+      }
+      if (cookie.indexOf(keyString) === 0) {
+        return cookie.substring(keyString.length, cookie.length);
+      }
+    }
+    return "";
+  }
+}
--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,72 @@
 
 function init() {
+  window.onbeforeunload = saveWebchatOpenState;
   $("#webchatBtn")[0].draggable = false;
+  $("#webchatSettingBtn")[0].draggable = false;
 
   webChatInit();
+  loadSetting();
+  loadWebchatOpenState();
+}
+
+function saveWebchatOpenState() {
+  new CookieUtil().set(
+      "webchatOpened",
+      ($("#webchatContainer")[0].hidden === false).toString(),
+      365
+  );
+}
+
+function loadWebchatOpenState() {
+  let rememberOpened = new CookieUtil().get("webchatRememberOpened");
+  if (rememberOpened === "true" &&
+      new CookieUtil().get("webchatOpened") === "true") {
+    webchatOpen();
+  }
+}
+
+function saveSetting() {
+  new CookieUtil().set(
+      "webchatUserName",
+      $("input[name='webchatUserNameTB']").val(),
+      365
+  );
+  new CookieUtil().set(
+      "webchatUserColor",
+      $("input[name='webchatUserColorCC']").val(),
+      365
+  );
+  new CookieUtil().set(
+      "webchatFontSize",
+      $("input[name='webchatFontSizeNB']").val(),
+      365
+  );
+  new CookieUtil().set(
+      "webchatRememberOpened",
+      $("input[name='webchatRememberOpenedCB']").is(":checked"),
+      365
+  );
+}
+
+function loadSetting() {
+  let userName = new CookieUtil().get("webchatUserName");
+  $("input[name='webchatUserNameTB']").val(
+      (userName !== "") ? (userName) : ("ME")
+  );
+
+  let userColor = new CookieUtil().get("webchatUserColor");
+  $("input[name='webchatUserColorCC']").val(
+      (userColor !== "") ? (userColor) : ("#3570A3")
+  );
+
+  let fontSize = new CookieUtil().get("webchatFontSize");
+  $("input[name='webchatFontSizeNB']").val(
+      (fontSize !== "") ? (fontSize) : ("12")
+  );
+
+  let rememberOpened = new CookieUtil().get("webchatRememberOpened");
+  $("input[name='webchatRememberOpenedCB']")[0].checked =
+      rememberOpened === "true";
 }
 
 function webchatOpen() {
@@ -13,4 +77,14 @@ function webchatOpen() {
 function webchatClose() {
   $("#webchatContainer")[0].hidden = true;
   $("#webchatBtnContainer")[0].hidden = false;
+}
+
+function webchatSettingOpen() {
+  $("#webchatSettingContainer")[0].hidden = false;
+  $("#webchatSettingBtn")[0].onclick = webchatSettingClose;
+}
+
+function webchatSettingClose() {
+  $("#webchatSettingContainer")[0].hidden = true;
+  $("#webchatSettingBtn")[0].onclick = webchatSettingOpen;
 }
