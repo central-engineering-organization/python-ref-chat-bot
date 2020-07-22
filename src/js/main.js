70aaf8376144c79fd67bf8da237bd2053d7bb912
peurocs4 <peurocs4@gmail.com>
2020-07-22 19:44:31 +0900

    Csk custom (#5)
    * webchat button hover styling
    
    - Button animation stops on hover-over
    
    * webchat close button hover style
    
    - Applied hover-over style on webchat close button
    
    * Added webchat setting dialog and cookie
    
    - Added an webchat setting dialog
    - Added a function to save/load setting from the cookie
    
    * Settings applied to actual webchat dialog
    
    - Added function that changes styles, etc. according to the setting's dialog

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,44 @@
-
+/**
+ * Runs at the document load
+ */
 function init() {
+  window.onbeforeunload = saveWebchatOpenState;
   $("#webchatBtn")[0].draggable = false;
+  $("#webchatSettingBtn")[0].draggable = false;
 
-  webChatInit();
+  loadSetting();
+  webChatInit(generateStyleDictionary());
+  applySetting();
 }
 
+/**
+ * Opens webchat dialog
+ */
 function webchatOpen() {
   $("#webchatContainer")[0].hidden = false;
   $("#webchatBtnContainer")[0].hidden = true;
 }
 
+/**
+ * Closes webchat dialog
+ */
 function webchatClose() {
   $("#webchatContainer")[0].hidden = true;
   $("#webchatBtnContainer")[0].hidden = false;
+}
+
+/**
+ * Opens setting's dialog
+ */
+function webchatSettingOpen() {
+  $("#webchatSettingContainer")[0].hidden = false;
+  $("#webchatSettingBtn")[0].onclick = webchatSettingClose;
+}
+
+/**
+ * Closes setting's dialog
+ */
+function webchatSettingClose() {
+  $("#webchatSettingContainer")[0].hidden = true;
+  $("#webchatSettingBtn")[0].onclick = webchatSettingOpen;
 }
